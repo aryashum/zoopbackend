8048f2a034b0251862b9c699473dd07f21a7c1a7
ashraywinash <61582642+ashraywinash@users.noreply.github.com>
2024-05-09 21:09:38 +0530

    formatting in userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,18 +2,20 @@ const express = require("express");
 
 const userRouter = express.Router();
 
-const {
-  createUserAccount,
-  checkIfUserExists,
-} = require("../controllers/userControl");
+const {createUserAccount,checkIfUserExists} = require("../controllers/userControl");
+
 const { getCartByStoreId, createCart } = require("../controllers/cartControl");
 
 // Define sub route for user signin
-userRouter.route("/signin").post(checkIfUserExists);
+userRouter.route("/signin")
+.post(checkIfUserExists);
 
 // Define sub route for user signup
-userRouter.route("/signup").post(createUserAccount);
+userRouter.route("/signup")
+.post(createUserAccount);
 
-userRouter.route("/cart").get(getCartByStoreId).post(createCart);
+userRouter.route("/cart")
+.get(getCartByStoreId)
+.post(createCart);
 
 module.exports = { userRouter };
